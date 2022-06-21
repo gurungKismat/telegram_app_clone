30243e00d459d213d256215e5d368f38eae32e62
gurungKismat <88021831+gurungKismat@users.noreply.github.com>
2022-06-21 17:41:11 +0545

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:telegram_app_clone/pages/root_app.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: RootApp(),
+  ));
+}
