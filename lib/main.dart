bef4bdee94bd5c7a1940033e72942d673e2846a4
gurungKismat <88021831+gurungKismat@users.noreply.github.com>
2022-06-21 19:30:56 +0545

    bottom navigation bar added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// @dart=2.9
+
 import 'package:flutter/material.dart';
 import 'package:telegram_app_clone/pages/root_app.dart';
 
--- lib/pages/root_app.dart
+++ lib/pages/root_app.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+
+import 'package:telegram_app_clone/theme/colors.dart';
 
 class RootApp extends StatefulWidget {
   const RootApp({Key? key}) : super(key: key);
@@ -8,8 +11,63 @@ class RootApp extends StatefulWidget {
 }
 
 class _RootAppState extends State<RootApp> {
+  int pageIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      bottomNavigationBar: getFooter(),
+    );
+  }
+
+  Widget getFooter() {
+    List iconsItem = [
+      MaterialIcons.account_circle,
+      Ionicons.ios_chatbubbles,
+      MaterialIcons.settings,
+    ];
+
+    List iconsLabel = ['Contacts', 'Chats', 'Settings'];
+
+    return Container(
+      height: 90,
+      width: double.infinity,
+      decoration: const BoxDecoration(
+        color: greyColor,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 10.0),
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: List.generate(
+              3,
+              (index) {
+                return Column(
+                  children: [
+                    Icon(
+                      iconsItem[index],
+                      size: 30,
+                      color:
+                          pageIndex == index ? primary : white.withOpacity(0.5),
+                    ),
+                    const SizedBox(
+                      height: 4.0,
+                    ),
+                    Text(
+                      iconsLabel[index],
+                      style: TextStyle(
+                        fontSize: 11,
+                        color: pageIndex == index
+                            ? primary
+                            : white.withOpacity(0.5),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            )),
+      ),
+    );
   }
 }
